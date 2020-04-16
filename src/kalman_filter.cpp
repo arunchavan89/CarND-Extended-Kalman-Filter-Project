05f20e62955f0ec764a53362b72d78c847880055
arunchavan89 <arunchavan89@gmail.com>
2020-04-16 12:07:41 +0200

    Adding first working pipeline for EKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,8 +3,8 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
+/*
+ * Please note that the Eigen library does not initialize
  *   VectorXd or MatrixXd objects with zeros upon creation.
  */
 
@@ -13,28 +13,28 @@ KalmanFilter::KalmanFilter() {}
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * predict the state
-   */
+    /**
+     * predict the state
+     */
     x_ = F_ * x_;
     MatrixXd Ft = F_.transpose();
     P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * update the state by using Kalman Filter equations
-   */
+    /**
+     * update the state by using Kalman Filter equations
+     */
     VectorXd z_pred = H_ * x_;
     VectorXd y = z - z_pred;
     MatrixXd Ht = H_.transpose();
@@ -51,53 +51,58 @@ void KalmanFilter::Update(const VectorXd &z) {
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * update the state by using Extended Kalman Filter equations
-   */
-    float px = x_(0);
-    float py = x_(1);
-    float vx = x_(2);
-    float vy = x_(3);
+    /**
+     * update the state by using Extended Kalman Filter equations
+     */
+    double px = x_(0);
+    double py = x_(1);
+    double vx = x_(2);
+    double vy = x_(3);
 
     //calculate readings from polar to cartesian
-    float rho = sqrt(px*px + vx*vy);
-    float phi = atan2(py, px);
-    float rho_dot;
+    double rho = sqrt((px * px) + (py * py));
+    double phi = atan2(py, px);
+    double rho_dot;
 
     // check if rho_dot is non negative value
-    if (rho_dot < 0)
+    if (rho_dot < 0.0)
     {
-      rho_dot = 0;
+        rho_dot = 0.0;
     }
     else
     {
-      rho_dot = (px*vx + py*vy) / rho;
+        rho_dot = (px*vx + py * vy) / rho;
     }
-    
+
     VectorXd z_pred(3);
     z_pred << rho, phi, rho_dot;
     VectorXd y = z - z_pred;
 
     /*
      * Normalizing Angles
-     * In C++, atan2() returns values between -pi and pi. 
-     * When calculating phi in y = z - h(x) for radar measurements, 
-     * the resulting angle phi in the y vector should be adjusted so that it is between -pi and pi. 
-     * The Kalman filter is expecting small angle values between the range -pi and pi. 
+     * In C++, atan2() returns values between -pi and pi.
+     * When calculating phi in y = z - h(x) for radar measurements,
+     * the resulting angle phi in the y vector should be adjusted so that it is between -pi and pi.
+     * The Kalman filter is expecting small angle values between the range -pi and pi.
      * HINT: when working in radians, you can add 2π or subtract 2π until the angle is within the desired range
     */
-    while ((y(1) < -M_PI) || (y(1) > M_PI))
+
+    if (y(1) < -M_PI)
     {
-      if (y(1) < -M_PI)
-      {
-        y(1) += 2*M_PI;
-      }
-      if (y(1) > M_PI) 
-      {
-       y(1) -= 2*M_PI;
-      }      
+        while (y(1) < -M_PI)
+        {
+            y(1) += 2 * M_PI;
+        }
     }
-    
+    if (y(1) > M_PI)
+    {
+        while (y(1) > M_PI)
+        {
+            y(1) -= 2 * M_PI;
+        }
+    }
+
+
     MatrixXd Ht = H_.transpose();
     MatrixXd S = H_ * P_ * Ht + R_;
     MatrixXd Si = S.inverse();
@@ -109,5 +114,5 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     long x_size = x_.size();
     MatrixXd I = MatrixXd::Identity(x_size, x_size);
     P_ = (I - K * H_) * P_;
-  
+
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -10,61 +10,62 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-   * Calculate the RMSE here.
-   */
-  VectorXd rmse(4);
-  rmse<<0, 0, 0, 0;
+    const vector<VectorXd> &ground_truth) {
+    /**
+     * Calculate the RMSE here.
+     */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
 
-  //check if estimation and ground-truth vectors are not empty
-  if((estimations.size() <= 0) || (ground_truth.size() <= 0) || (estimations.size() != ground_truth.size()))
-  {
-     printf("Error: Either estimations or ground_truth vector is empty.!");
-  }
-  else
-  {
-     for (int i = 0; i < estimations.size(); i++)
-     {
-        VectorXd diff = estimations[i] - ground_truth[i];
-        diff = diff.array() * diff.array();
-        rmse += diff;
-     }
-     // : calculate the mean
-     rmse = rmse/estimations.size();
-     // : calculate the squared root
-     //rmse = rmse.sqrt();
-  }
+    //check if estimation and ground-truth vectors are not empty
+    if ((estimations.size() <= 0) || (ground_truth.size() <= 0))
+    {
+        printf("Error: Either estimations or ground_truth vector is empty.!");
+        return rmse;
+    }
+    else
+    {
+        for (int i = 0; i < estimations.size(); i++)
+        {
+            VectorXd diff = estimations[i] - ground_truth[i];
+            diff = diff.array() * diff.array();
+            rmse += diff;
+        }
+        // : calculate the mean
+        rmse = rmse / estimations.size();
+        // : calculate the squared root
+        rmse = rmse.array().sqrt();
+    }
 
-  return rmse;
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * Calculate a Jacobian here.
-   */
-  MatrixXd Hj(3,4);
-  // recover state parameters
-  float px = x_state(0);
-  float py = x_state(1);
-  float vx = x_state(2);
-  float vy = x_state(3);
+    /**
+     * Calculate a Jacobian here.
+     */
+    MatrixXd Hj(3, 4);
+    // recover state parameters
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
 
-  // YOUR CODE HERE 
-  float c1 = px*px + py*py;
-  float c2 = sqrt(c1);
-  float c3 = c1*c2;
-  
-  // check division by zero
-  if (fabs(c1) < 0.0001) {
-    std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+    // YOUR CODE HERE 
+    double c1 = px * px + py * py;
+    double c2 = sqrt(c1);
+    double c3 = c1 * c2;
+
+    // check division by zero
+    if (fabs(c1) < 0.0001) {
+        std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+        return Hj;
+    }
+
+    // compute the Jacobian matrix
+    Hj << (px / c2), (py / c2), 0, 0,
+        -(py / c1), (px / c1), 0, 0,
+        py*(vx*py - vy * px) / c3, px*(px*vy - py * vx) / c3, px / c2, py / c2;
+
     return Hj;
-  }
-  
-  // compute the Jacobian matrix
-  Hj<<(px/c2), (py/c2), 0, 0,
-  -(py/c1), (px/c1), 0, 0,
-      py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
-  
-  return Hj;
 }
