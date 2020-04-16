01da98545db98c2a25562cdba56ec41edb1c4e46
arunchavan89 <arunchavan89@gmail.com>
2020-04-16 14:15:57 +0200

    Revrting back changes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,7 @@ int main() {
         });
 
     int port = 4567;
-    if (h.listen("127.0.0.1", port)) {
+    if (h.listen(port)) {
         std::cout << "Listening to port " << port << std::endl;
     }
     else {
